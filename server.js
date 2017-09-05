63b7234c5d935e1c2a468298aea390388a81eaad
rupaizmocars <31019343+rupaizmocars@users.noreply.github.com>
2017-09-05 11:49:01 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('article-one', function(req, res){
+app.get('article-one', function (req, res){
 res.send('article one responded and will be served here');
 });
 
 
-app.get('article-two', function(req, res){
+app.get('article-two', function (req, res){
 res.send('article two responded and will be served here');
 });
 
 
-app.get('article-three', function(req, res){
+app.get('article-three', function (req, res){
 res.send('article three responded and will be served here');
 });
 
