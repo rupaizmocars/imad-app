9e1f0d2fc5eca36f6b0aec09918372b800d03073
rupaizmocars <31019343+rupaizmocars@users.noreply.github.com>
2017-09-07 12:52:31 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML= "new value"
 
 
 //move the img
-var img= document.getElementById("img");
+var img= document.getElementById("madi");
 img.onclick=function (){
     img.style.marginleft='100px';
     
