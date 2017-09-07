8c80234e1d13a299f3f43839c9fc6e0b085c200a
rupaizmocars <31019343+rupaizmocars@users.noreply.github.com>
2017-09-07 11:39:39 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,11 @@ console.log('Loaded!');
 //change the text
 var element=document.getElementById("main-text");
 element.innerHTML= "new value"
+
+
+//move the img
+var img=document.getelementbyid("img");
+img.onclick=function (){
+    img.style.marginleft='100px';
+    
+};
